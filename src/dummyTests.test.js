915f5a694d2a728041c6b8c35c08d889f73a7b13
Vaclav Malinak <vaclavm@kentico.com>
2018-07-13 13:25:18 +0200

    Install and configure JEST

--- src/dummyTests.test.js
+++ src/dummyTests.test.js
@@ -10,20 +10,22 @@ describe('Basic math', () => {
 
 describe('Promise resolving', () => {
   it('resolves with correct result - constructor', () => {
-    const myPromise = new Promise(resolve => resolve(42));
+    const myPromise = new Promise((resolve, reject) => {
+      resolve(42);
+    });
 
     return myPromise.then(result => expect(result).toBe(42));
   });
 
   it('resolves with correct result - shorthand syntax', () => {
     return Promise.resolve(42)
-      .then(result => expect(result).toBe(42));
+      .then((result) => expect(result).toBe(42));
   });
 
   it('resolves with correct result - more assertions', () => {
-    const myPromise = new Promise(resolve => resolve(42));
+    const myPromise = new Promise((resolve, reject) => resolve(42));
 
-    return myPromise.then((result) => {
+    return myPromise.then(result => {
       expect(result).toBeLessThan(666);
       expect(result).toBeGreaterThanOrEqual(-1 / 12);
       expect(result).toBe(42);
@@ -33,35 +35,49 @@ describe('Promise resolving', () => {
 
 describe('Promise rejection', () => {
   it('rejects with error - constructor', () => {
-    const myPromise = new Promise((resolve, reject) => reject(Error('Ooops...')));
+    const myPromise = new Promise((resolve, reject) => {
+      reject('Ooops...');
+    });
 
     return myPromise
-      .then(() => fail('This should never happen. Catch block should be called immediately'))
-      .catch(error => expect(error.message).toBe('Ooops...'));
+      .then(result => {
+        fail('This should never happen. Catch block should be called immediately')
+      })
+      .catch(error => {
+        expect(error).toBe('Ooops...');
+      });
   });
 
   it('rejects with error - shorthand syntax', () => {
-    return Promise.reject(Error('Ooops...'))
-      .then(() => fail('This should never happen. Catch block should be called immediately'))
-      .catch(error => expect(error.message).toBe('Ooops...'));
+    return Promise.reject('Ooops...')
+      .then(result => {
+        fail('This should never happen. Catch block should be called immediately')
+      })
+      .catch(error => {
+        expect(error).toBe('Ooops...');
+      });
   });
 
   it('throws and error falls to catch block', () => {
-    const chainedPromise = new Promise(resolve => resolve(-1))
-      .then((result) => {
-        // if result is negative, throw => first chained catch block will be called all inbetween thens will be ignored
-        if (result < 0) {
-          throw new Error(`${result} is less than zero`);
-        }
+    const chainedPromise = new Promise((resolve, reject) => {
+      resolve(-1);
+    }).then(result => {
+      // if result is negative, throw => first chained catch block will be called all inbetween thens will be ignored
+      if (result < 0) {
+        throw new Error(`${result} is less than zero`);
+      }
 
-        // otherwise pass the result to next then
-        Promise.resolve(result);
-      });
+      // otherwise pass the result to next then
+      resolve(result);
+    });
 
     return chainedPromise
-      .then(() => {
+      .then(result => {
+        console.log('Then block', result);
         fail('Then block should not be called');
       })
-      .catch(error => expect(error.toString().indexOf('-1') > -1).toBeTruthy());
+      .catch(error => {
+        expect(error.toString().indexOf('-1') > -1).toBeTruthy();
+      });
   });
 });
