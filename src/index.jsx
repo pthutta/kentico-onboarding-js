22d87a53ea23240fe2b01c5c82d55943ccdada09
Peter Hutta <PeterH@kentico.com>
2018-08-22 15:26:22 +0200

    KA-364 Instantiate store in index.jsx, use Provider to wrap App

--- src/index.jsx
+++ src/index.jsx
@@ -2,11 +2,22 @@ require.context('../public/', true);
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
 import ReactDom from 'react-dom';
 import React from 'react';
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux';
+import { logger } from 'redux-logger'
+import { todoApp } from './tmpName/reducers';
 
 import { App } from './App.jsx';
 
+const store = createStore(
+  todoApp,
+  applyMiddleware(logger)
+);
+
 ReactDom.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('app-root'));
