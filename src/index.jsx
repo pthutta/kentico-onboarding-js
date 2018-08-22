c464c09a46cfffe6a91c9b93f81f8ab689de33fd
Peter Hutta <PeterH@kentico.com>
2018-08-22 17:20:30 +0200

    KA-364 Use redux-devtools-extension package

--- src/index.jsx
+++ src/index.jsx
@@ -2,16 +2,16 @@ require.context('../public/', true);
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
 import ReactDom from 'react-dom';
 import React from 'react';
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import { logger } from 'redux-logger';
 import { todoApp } from './tmpName/reducers';
 import { App } from './App';
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
   todoApp,
-  composeEnhancers(applyMiddleware(logger))
+  composeWithDevTools(applyMiddleware(logger))
 );
 
 ReactDom.render(
