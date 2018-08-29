c1d183f4b5413cff4a5136d196838fbd70174f8e
Peter Hutta <PeterH@kentico.com>
2018-08-29 14:43:06 +0200

    KA-365 Migrate index.jsx to TypeScript

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,23 @@
 require.context('../public/', true);
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
-import ReactDom from 'react-dom';
-import React from 'react';
-import { createStore, applyMiddleware } from 'redux';
+import * as ReactDom from 'react-dom';
+import * as React from 'react';
+import { createStore, applyMiddleware, Store } from 'redux';
 import { Provider } from 'react-redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { logger } from 'redux-logger';
 import { OrderedMap } from 'immutable';
-import { todoApp } from './reducers/todoApp.ts';
-import { App } from './App.tsx';
+import { todoApp } from './reducers/todoApp';
+import { App } from './App';
+import { IAppState } from './models/IAppState';
 
-const initialState = {
+const initialState: IAppState = {
   list: {
     items: OrderedMap()
   }
 };
 
-const store = createStore(
+const store: Store = createStore(
   todoApp,
   initialState,
   composeWithDevTools(applyMiddleware(logger))
