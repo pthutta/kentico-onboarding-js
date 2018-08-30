a69584b5a8d86707f9fed26bb35ba8f708091323
Peter Hutta <PeterH@kentico.com>
2018-08-30 16:57:50 +0200

    Initialize KeyMap

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,24 +1,32 @@
 import '../styles/sticky-footer.css';
 import * as React from 'react';
 import { List } from '../containers/List';
+import { HotKeys, KeyMap } from 'react-hotkeys';
+
+const map: KeyMap = {
+  'confirm': 'enter',
+  'cancelEditing': 'escape',
+  'deleteItem': 'command+d',
+};
 
 export const App: React.StatelessComponent = (): JSX.Element => (
-  <div>
-    <div className="container">
-      <div className="header clearfix">
-        <h3 className="text-muted">
-          Kentico Academy
-        </h3>
+  <HotKeys keyMap={map}>
+    <div>
+      <div className="container">
+        <div className="header clearfix">
+          <h3 className="text-muted">
+            Kentico Academy
+          </h3>
+        </div>
+        <section id="app-content">
+          <List />
+        </section>
       </div>
-
-      <section id="app-content">
-        <List />
-      </section>
+      <footer className="footer">
+        <p>
+          &copy; 2017 Kentico software, s.r.o
+        </p>
+      </footer>
     </div>
-    <footer className="footer">
-      <p>
-        &copy; 2017 Kentico software, s.r.o
-      </p>
-    </footer>
-  </div>
+  </HotKeys>
 );
